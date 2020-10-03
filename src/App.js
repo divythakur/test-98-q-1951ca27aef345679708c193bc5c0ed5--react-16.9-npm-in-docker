2a68f480d331ef990db24b38f57208e77aa28ed9
divythakur <divyanshuthakur75@gmail.com>
2020-10-03 18:43:19 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import data from './data.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const movies = data.items;
+
+
+class App extends Component {
+  componentDidMount() {
+    console.log((data.items))
+  }
+
+
+  render() {
+    return (
+      <div >
+        <h3 className="App" style={{ position:"fixed",top:"0",height:"50px",width:"100%",left:"50%",color:"darkblue"}}>Movies In Banglore</h3>
+        <div style={{ display: "flex", justifyContent: "space-between",marginTop:"50px" }}>
+          {
+
+            data.items.map((e) => {
+              return (
+                <div id={`movie-card-${e.movie_id}`} style={{ display: "flex", justifyContent: "space-between" }}>
+                  <Card style={{ margin: "20px", width: "104%" }} >
+                    <CardActionArea>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {e.movie_name}
+                      </Typography>
+                      <a href={e.movie_trailer}>
+                        <CardMedia
+                          component="img"
+                          alt="Movie"
+                          height="300"
+
+                          image={e.movie_poster}
+                          title="Movie"
+                          style={{ width: "300px" }} /></a>
+                      <CardContent>
+
+                        <Typography variant="body2" color="textSecondary" component="p">
+                          {e.movie_overview}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </div>
+
+              );
+            })
+
+          }
+        </div>
+      </div>
+
+
+
+    );
+  }
 }
-
-export default App;
+export default App
